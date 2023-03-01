18ef7a9ef6b8979539e27a0b7d1e5e4d479b1a9c
Gor <61106836+TheGor-365@users.noreply.github.com>
2023-03-01 16:46:18 +0300

    Add select user and tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:body, :user_id)
+    params.require(:post).permit(:body, :user_id, tag_ids: [])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_130911) do
   end
 
   create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
